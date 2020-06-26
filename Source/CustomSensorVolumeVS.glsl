3235e35733bc98ff69f7ebc7ed26bf3dad5b5d54
MathDotSqrt <ctrenkov@gmail.com>
2020-06-26 13:52:28 -0400

    removed projection code to a different function

--- Source/CustomSensorVolumeVS.glsl
+++ Source/CustomSensorVolumeVS.glsl
@@ -5,16 +5,29 @@ varying vec3 v_positionWC;
 varying vec3 v_positionEC;
 varying vec3 v_normalEC;
 
+const float EARTH_CIRCUMFERENCE = 40075000.;
+const float RADIAN_TO_METERS = EARTH_CIRCUMFERENCE / czm_twoPi;
+const float EARTH_RADIUS = 6371000.;
+
+vec4 computeEquirectangular(vec4 world_pos){
+	float radius = length(world_pos.xyz);
+	float lat = asin(world_pos.z / radius) * RADIAN_TO_METERS;
+	float lon = atan(world_pos.y, world_pos.x) * RADIAN_TO_METERS;
+
+	return vec4(radius - EARTH_RADIUS, lon, lat, 1);
+}
+
 vec4 computePosition(vec4 position3D){
 	
 	vec4 p = czm_model * position3D;
 	if(czm_sceneMode == czm_sceneMode3D){
 		return p;
 	}
-	float radius = length(p.xyz);
-	float lat = asin(p.z / radius) * 40075000. / ( 2. * 3.14159);
-	float lon = atan(p.y, p.x) * 40075000. / (2. * 3.14159);
-	return vec4(radius - 6371000., lon, lat, 1);
+
+
+	vec4 projected_pos = computeEquirectangular(p);
+
+	return mix(projected_pos, p, czm_morphTime);
 }
 
 void main()
