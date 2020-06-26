58278b8fbed29ff4becad297ab4cd0eb94359b6a
MathDotSqrt <ctrenkov@gmail.com>
2020-06-25 23:51:08 -0400

    now works. Kinda, need to clamp and do other fancy stuff

--- Source/CustomSensorVolumeVS.glsl
+++ Source/CustomSensorVolumeVS.glsl
@@ -5,9 +5,26 @@ varying vec3 v_positionWC;
 varying vec3 v_positionEC;
 varying vec3 v_normalEC;
 
+vec4 computePosition(vec4 position3D){
+	
+	vec4 p = czm_model * position3D;
+	if(czm_sceneMode == czm_sceneMode3D){
+		return p;
+	}
+
+	float radius = length(p.xyz);
+	float lat = asin(p.z / radius) * 40075000. / ( 2. * 3.14159);
+	float lon = atan(p.y, p.x) * 40075000. / (2. * 3.14159);
+	return vec4(radius / 10., lon, lat, 1);
+}
+
 void main()
 {
-    gl_Position = czm_modelViewProjection * position;
+
+    vec4 world_pos = computePosition(position);
+
+
+    gl_Position = czm_viewProjection * world_pos;
     v_positionWC = (czm_model * position).xyz;
     v_positionEC = (czm_modelView * position).xyz;
     v_normalEC = czm_normal * normal;
